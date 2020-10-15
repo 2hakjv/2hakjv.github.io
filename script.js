d2cc5d2c0deb855958d21a256301c475bc18ba7f
2hakjv <61584406+2hakjv@users.noreply.github.com>
2020-10-16 08:52:52 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-var url = "https://api.minetools.eu/ping/tometo.cf";
-$.getJSON(url, function (r) {
-    if (r.error) {
-        $('.box').css('background', '#e74c3c');
-        $('h2').html('서버 닫힘');
-        $('#status').html('(_　_)。゜zｚＺ');
-        return false;
-    } else {
-        $('.box').css('background', '#2fcc66');
-        $('h2').html('서버 열림');
-        $('#status').html('플레이어: ' + r.players.online + '/' + r.players.max);
-    }
-});
+var url="https://api.minetools.eu/ping/tometo.cf";$.getJSON(url,function(r){if(r.error){$('.box').css('background','#e74c3c');$('h2').html('서버 닫힘');$('#status').html('(_　_)。゜zｚＺ');return false}else{$('.box').css('background','#2fcc66');$('h2').html('서버 열림');$('#status').html('플레이어: '+r.players.online+'/'+r.players.max)}});
